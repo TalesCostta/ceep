07078dd4e52f6292ac848c22fc8b15075c859884
TalesCostta <talessilvacostta@gmail.com>
2022-03-16 12:58:05 -0300

    Adicionado Alguns componentes

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import CardNota from "./CardNota";
+
+export default class ListaDeNotas extends Component {
+    render() {
+        return (
+            <section>
+                <ul>
+                    <li>
+                        <CardNota />
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <CardNota />
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <CardNota />
+                    </li>
+                </ul>
+            </section>
+        );
+    }
+}
